3ec02305776121a1e6011a0cb4f4145b0ee76209
João Martins <88497664+JoaoMlMartins@users.noreply.github.com>
2023-12-09 19:41:03 +0000

    dashboard update

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -4,19 +4,6 @@ import React, { useEffect, useState } from 'react';
 
 
 function App() {
-  const [refreshCount, setRefreshCount] = useState(0);
-
-  useEffect(() => {
-    // Set up an interval to make requests every 1 second (1000 milliseconds)
-    const intervalId = setInterval(() => {
-      // Increment the refreshCount to trigger a re-render
-      setRefreshCount(prevCount => prevCount + 1);
-    }, 5000);
-
-    // Clear the interval when the component unmounts to avoid memory leaks
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <div>
       <div className="container mt-3">
