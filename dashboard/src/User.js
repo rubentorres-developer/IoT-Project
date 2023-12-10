a413432104866cacb46c9e888d421ded851bb28c
João Martins <88497664+JoaoMlMartins@users.noreply.github.com>
2023-12-10 00:19:07 +0000

    updating dashboard v21.1.0

--- dashboard/src/User.js
+++ dashboard/src/User.js
@@ -6,14 +6,19 @@ import ColumnChart from './ColumnChart';
 
 const User = () => {
   const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   // Function to fetch data from the API
   const fetchData = async () => {
     try {
       const response = await axios.get(`http://processor:8082/data`);
       setUserData(response.data);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching data:', error);
+      setLoading(false);
+      setError(error);
     }
   };
 
